661b30cd682b72d454a8ac2968e8d04b131266a7
zibewgit <abubakr@zibew.com>
2022-01-30 04:54:18 +0530

    razor expressions

---
+++ FirstWebApp/Controllers/RazorExpressionsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class RazorExpressionsController : Controller
+    {
+        // GET: RazorExpressions
+        public ActionResult Index()
+        {
+            ViewBag.studName = "omair";
+            ViewBag.studRoll = 001;
+            ViewBag.studMk = 31;
+            ViewBag.emotList = new List<string>() { "🙈", "🙉", "🙊", "🐒", "🐵" };
+            return View();
+        }
+    }
+}
