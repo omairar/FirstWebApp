bff88d1115f786d91243450d47a9f4fd5b9402d6
zibewgit <abubakr@zibew.com>
2022-01-31 02:30:51 +0530

    Http objects done

---
+++ FirstWebApp/Controllers/HttpObjectsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HttpObjectsController : Controller
+    {
+        // GET: HttpObjects
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getRequestObjects()
+        {
+            ViewBag.reqURL = Request.Url;
+            ViewBag.reqPAP = Request.PhysicalApplicationPath;
+            ViewBag.reqP = Request.Path;
+            ViewBag.reqBT = Request.Browser.Type;
+            ViewBag.reqQS = Request.QueryString["n"];
+            ViewBag.reqH = Request.Headers["Accept"];
+            ViewBag.reqHTM = Request.HttpMethod;
+            return View();
+        }
+
+        public ActionResult setResponseObjects()
+        {
+            Response.Write("Hello world");
+            Response.ContentType = "text/html";
+            Response.Headers["Server"] = "My server";
+            Response.StatusCode = 500;
+            
+            return View();
+        }
+    }
+}
